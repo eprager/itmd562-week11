ebee4da07ce405475578ea25ff4224a693afe8b9
Emma Prager <eprager@hawk.iit.edu>
2019-11-09 17:27:37 -0600

    error handling

--- project/app.js
+++ project/app.js
@@ -5,4 +5,19 @@ const userRoutes = require('./api/routes/users');
 
 app.use('/users', userRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
